3264eff929d52aafae01e4daeefb3675a468386f
Rayane Mksm <rayanemksm5@gmail.com>
2025-11-25 16:37:44 +0100

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
 export default {
   plugins: {
-<<<<<<< HEAD
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-=======
     "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
 };
 
->>>>>>> 7d7b75f (fix)
